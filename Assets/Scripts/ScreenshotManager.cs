4578ec558526bac69fad973fb8e783567d2bb968
michelkohl <michel-kohl@t-online.de>
2020-05-31 12:21:02 +0200

    Initial Unity Project

---
+++ Assets/Scripts/ScreenshotManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotManager : MonoBehaviour
+{
+    const string FilePath = @"D:\Data\Uni\SG-P10\Judas.png";
+
+    public void CaptureScreenShot()
+    {
+        ScreenCapture.CaptureScreenshot(FilePath);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            CaptureScreenShot();
+    }
+}
