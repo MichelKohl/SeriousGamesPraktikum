e62a8c7e459a43383c7fe5ffbc50529b4be5676f
Michail Igorevic Pedko <macpedko@gmail.com>
2020-06-04 15:28:43 +0200

    Gameplay 1

---
+++ Assets/Mini Games/Snake/_Scripts/Food.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    [SerializeField] private int points;
+    [SerializeField] private GameObject borders;
+    [SerializeField] private float offset;
+
+    private float minX = 0, minY = 0;
+    private int xUnits = 0, yUnits = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      float maxX = 0, maxY = 0;
+      foreach(Transform borderTransform in borders.transform){
+        Vector3 position = borderTransform.position;
+        minX = Mathf.Min(minX, position.x);
+        minY = Mathf.Min(minY, position.y);
+        maxX = Mathf.Max(maxX, position.x);
+        maxY = Mathf.Max(maxY, position.y);
+      }
+      xUnits = (int) ((maxX - minX) / offset);
+      yUnits = (int) ((maxY - minY) / offset);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider){
+      transform.position = GetRandomPosition();
+      //TODO points etc.
+      collider.gameObject.transform.parent.gameObject.GetComponent<Snake>().Eat();
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+      return new Vector3( minX + Mathf.FloorToInt(Random.Range(1, xUnits)) * offset,
+                          minY + Mathf.FloorToInt(Random.Range(1, yUnits)) * offset, 0);
+    }
+}
--- Assets/Mini Games/Snake/_Scripts/Snake.cs
+++ Assets/Mini Games/Snake/_Scripts/Snake.cs
@@ -8,15 +8,18 @@ public class Snake : MonoBehaviour
     [SerializeField] private Joystick joystick;
     [SerializeField] private float movement;
     [SerializeField] private float startUpdateFrequ;
+    [SerializeField] private GameObject bodypart;
 
     private Vector3 direction;
     private float updateFrequency, counter;
     private GameObject head;
     private List<GameObject> body;
+    private bool eating;
     // Start is called before the first frame update
     void Start()
     {
       counter = 0;
+      eating = false;
       updateFrequency = startUpdateFrequ;
       direction = new Vector3(0,1,0);
       head = GameObject.Find("Head");
@@ -25,17 +28,15 @@ public class Snake : MonoBehaviour
       body.Add(GameObject.Find("Bodypart 1"));
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
       if(counter > updateFrequency){
         UpdateDirection();
         Move();
         counter = 0;
       }
-      counter += Time.deltaTime;
+      counter += Time.fixedDeltaTime;
     }
-
     void UpdateDirection()
     {
       float horizontal = joystick.Horizontal, vertical = joystick.Vertical;
@@ -51,12 +52,22 @@ public class Snake : MonoBehaviour
     void Move()
     {
       Transform headTransform = head.transform;
-      GameObject lastBodypart = body[0];
-      lastBodypart.transform.position =  headTransform.position;
-      lastBodypart.transform.up = headTransform.up;
-      body.RemoveAt(0);
-      body.Add(lastBodypart);
+      if(eating){
+        eating = false;
+        body.Add(Instantiate(bodypart, headTransform.position, headTransform.rotation, transform));
+      } else {
+        GameObject lastBodypart = body[0];
+        lastBodypart.transform.position =  headTransform.position;
+        lastBodypart.transform.up = headTransform.up;
+        body.RemoveAt(0);
+        body.Add(lastBodypart);
+      }
       head.transform.position += movement * direction;
       head.transform.up = direction;
+    }
+
+    public void Eat()
+    {
+      eating = true;
     }
 }
