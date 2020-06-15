f2f47759c9e94c55a500e37a604fcd85e7726a5d
Tom Halecker <tom.halecker@dxc.com>
2020-06-15 22:07:59 +0200

    initial popup and scene swap

---
+++ Assets/Scripts/ShowGamePopUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowGamePopUp : MonoBehaviour
+{
+    private void OnMouseDown() {
+        var gamePopUp = GameManager.INSTANCE.GamePopUp;
+        var miniGameStarter = gamePopUp.GetComponent<MiniGameStarter>();
+        
+        miniGameStarter.miniGame = GameManager.INSTANCE.GetRandomMiniGame();
+
+        // Activate gamePopUp in hirarchy after setting it up
+        gamePopUp.SetActive(true);
+    }
+}
