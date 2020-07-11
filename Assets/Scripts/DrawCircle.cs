14b2e722ff937d57e876a70f65ec827c1097d7fa
michelkohl <michel-kohl@t-online.de>
2020-07-11 11:43:04 +0200

    Transport POIs / Visual Radius and Collider

---
+++ Assets/Scripts/DrawCircle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DrawCircle : MonoBehaviour
+{
+    [Range(0,50)]
+    public int segments = 50;
+    [Range(0,50)]
+    public float xradius = 5;
+    [Range(0,50)]
+    public float yradius = 5;
+    LineRenderer line;
+    public float xOffset = -14;
+    public float yOffset = 0;
+
+    void Start ()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.positionCount = segments + 1;
+        line.useWorldSpace = false;
+        CreatePoints ();
+    }
+
+    void CreatePoints ()
+    {
+        float x;
+        float y;
+        float z;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin (Mathf.Deg2Rad * angle) * xradius + xOffset;
+            y = Mathf.Cos (Mathf.Deg2Rad * angle) * yradius + yOffset;
+
+            line.SetPosition (i,new Vector3(x,y,0) );
+
+            angle += (360f / segments);
+        }
+    }
+}
--- Assets/Scripts/ShowGamePopUp.cs
+++ Assets/Scripts/ShowGamePopUp.cs
@@ -6,13 +6,43 @@ using TMPro;
 
 public class ShowGamePopUp : MonoBehaviour
 {
+    private bool inRange = false;
+    public Transform rangeCollider;
     private void OnMouseDown() {
-        var gamePopUp = GameManager.INSTANCE.GamePopUp;
-        var miniGameStarter = gamePopUp.GetComponent<MiniGameStarter>();
-        
-        miniGameStarter.miniGame = GameManager.INSTANCE.GetRandomMiniGame();
+        if (inRange)
+        {
+            var gamePopUp = GameManager.INSTANCE.GamePopUp;
+            var miniGameStarter = gamePopUp.GetComponent<MiniGameStarter>();
 
-        // Activate gamePopUp in hirarchy after setting it up
-        gamePopUp.SetActive(true);
+            miniGameStarter.miniGame = GameManager.INSTANCE.GetRandomMiniGame();
+
+            // Activate gamePopUp in hirarchy after setting it up
+            gamePopUp.SetActive(true);
+        }   
+        
+    }
+
+    /// <summary>
+    /// Sets a boolean if the player enters the range/collider of the POI
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    /// <summary>
+    /// Sets a boolean if the player exits the range/collider of the POI
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+        }
     }
 }
