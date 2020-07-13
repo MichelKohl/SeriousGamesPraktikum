f9caf9e8bd117e163184f90642098df43016ef65
Fabian Kast <fabian_kast@yahoo.de>
2020-07-13 18:11:47 +0200

    distance between player and locationobject changed from kepsilon to 0.1f

--- Assets/Scripts/WalkTowardsLocation.cs
+++ Assets/Scripts/WalkTowardsLocation.cs
@@ -13,7 +13,7 @@ public class WalkTowardsLocation : MonoBehaviour
     {
         LocationObjectPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z);
         //transform.position = Vector3.MoveTowards(transform.position, wayPointPos, walkSpeed * Time.deltaTime);
-        if (Vector3.Distance(LocationObjectPos, transform.position) > Vector3.kEpsilon)
+        if (Vector3.Distance(LocationObjectPos, transform.position) > 0.1f)
         {
             AnimationController.SetBool("Walk", true);
             transform.position = Vector3.MoveTowards(transform.position, LocationObjectPos, walkSpeed * Time.deltaTime);
