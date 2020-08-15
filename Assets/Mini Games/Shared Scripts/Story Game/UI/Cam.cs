433246d4d4286e12e386acbf56033a91217d7d6c
Michail Igorevic Pedko <macpedko@gmail.com>
2020-08-15 20:09:06 +0200

    Create Cam.cs

---
+++ Assets/Mini Games/Shared Scripts/Story Game/UI/Cam.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    [SerializeField] private Transform thirdPersonCam;
+    [SerializeField] private float moveStep = 1f;
+    [SerializeField] private float moveDelta = 0.001f;
+    [SerializeField] private float rotateStep = 1f;
+    [SerializeField] private float rotateDelta = 0.001f;
+
+    private Vector3 firstPersonPos;
+    private Quaternion firstPersonQuat;
+
+    private Vector3 moveToPosition;
+    private Quaternion rotateToRotation;
+    private bool positionSet = true;
+    private bool firstPerson = true;
+
+    private void Start()
+    {
+        firstPersonPos = transform.position;
+        firstPersonQuat = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO delete next two lines, which are for testing purposes
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeToFirstPerson();
+        if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeToThirdPerson();
+
+        if (!positionSet)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, moveToPosition, moveStep);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateToRotation, rotateStep);
+
+            if (Approx(transform.rotation, rotateToRotation, rotateDelta) &&
+                Approx(transform.position, moveToPosition, moveDelta))
+            {
+                positionSet = true;
+                firstPerson = !firstPerson;
+            }
+
+        }
+        
+    }
+
+    public void ChangeToFirstPerson()
+    {
+        if (positionSet && !firstPerson)
+        {
+            moveToPosition = firstPersonPos;
+            rotateToRotation = firstPersonQuat;
+            positionSet = false;
+        }
+    }
+
+    public void ChangeToThirdPerson()
+    {
+        if (positionSet && firstPerson)
+        {
+            moveToPosition = thirdPersonCam.position;
+            rotateToRotation = thirdPersonCam.rotation;
+            positionSet = false;
+        }
+    }
+
+    private bool Approx(Quaternion current, Quaternion target, float delta)
+    {
+        return Quaternion.Dot(current, target) > 1f - delta;
+    }
+
+    private bool Approx(Vector3 current, Vector3 target, float delta)
+    {
+        return Vector3.Distance(current, target) < delta;
+    }
+}
